978bcc2f8b9b2da77e69950ba17ee9c29eeb36bd
rociogim23 <141087441+rociogim23@users.noreply.github.com>
2023-08-07 22:09:34 -0300

    cambio2

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// Obtener referencia al div
+const divConBoton = document.querySelector('.boton-con-fondo');
+
+// Agregar el manejador de eventos al hacer clic en el div
+divConBoton.addEventListener('click', function () {
+    alert("Hola! Soy el div");
+});
